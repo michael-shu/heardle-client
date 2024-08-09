f631617c269f99c4d4bba2e49deff618736bac86
michael-shu <michaelshu34@gmail.com>
2024-08-09 16:46:24 -0400

    change navbar

--- app/home/Navbar.tsx
+++ app/home/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import Image from 'next/image';
+import Image from "next/image";
 
 const Navbar = () => {
   return (
@@ -11,30 +11,55 @@ const Navbar = () => {
             <Image alt="logo" src="/logo.svg" width={50} height={75} className="rounded-full" />
             <h1 className="text-white text-2xl font-semibold">Heardle</h1>
           </div>
-          <ul className="hidden md:flex gap-x-8 text-white">
+
+          {/* Hamburger Menu */}
+          <input type="checkbox" id="menu-toggle" className="hidden" />
+          <label htmlFor="menu-toggle" className="cursor-pointer md:hidden block">
+            <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </label>
+
+          {/* Menu Links */}
+          <ul className="hidden md:flex gap-x-8 text-white md:block" id="menu">
             <li className="hover:text-gray-300 transition duration-300">
-              <Link href="/home">
-                Home
-              </Link>
+              <Link href="/home">Home</Link>
             </li>
             <li className="hover:text-gray-300 transition duration-300">
-              <Link href="/home/user">
-                User
-              </Link>
+              <Link href="/home/user">User</Link>
             </li>
             <li className="hover:text-gray-300 transition duration-300">
-              <Link href="/home/play">
-                Play
-              </Link>
+              <Link href="/home/play">Play</Link>
             </li>
             <li className="hover:text-gray-300 transition duration-300">
-              <Link href="/home/about">
-                About
-              </Link>
+              <Link href="/home/about">About</Link>
             </li>
           </ul>
         </div>
       </div>
+
+      {/* Mobile Menu */}
+      <ul className="bg-emerald-800 text-white absolute left-0 top-20 w-full hidden" id="menu">
+        <li className="hover:bg-emerald-700 p-4">
+          <Link href="/home">Home</Link>
+        </li>
+        <li className="hover:bg-emerald-700 p-4">
+          <Link href="/home/user">User</Link>
+        </li>
+        <li className="hover:bg-emerald-700 p-4">
+          <Link href="/home/play">Play</Link>
+        </li>
+        <li className="hover:bg-emerald-700 p-4">
+          <Link href="/home/about">About</Link>
+        </li>
+      </ul>
+
+      {/* Mobile Menu Toggle */}
+      <style jsx>{`
+        #menu-toggle:checked + label + #menu {
+          display: block;
+        }
+      `}</style>
     </div>
   );
 };
