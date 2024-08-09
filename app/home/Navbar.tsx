a3affeef085369b5c782e10ccc54e50b28d1bc6e
michael-shu <michaelshu34@gmail.com>
2024-08-08 22:24:29 -0400

    lets make the navbar look better

--- app/home/Navbar.tsx
+++ app/home/Navbar.tsx
@@ -4,39 +4,38 @@ import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
-        <div className="container mx-auto px-4 h-full">
-          <div className="flex justify-between items-center h-full">
-            <ul className="hidden md:flex gap-x-6 text-white">
-            <li>
-                <Image alt="logo" src="/logo.svg" width={50} height={75}/>
-              </li>
-                <li>
-                <Link href = '/home'>
-                  <p>Home</p>
-                </Link>
-              </li>
-              <li>
-                <Link href = '/home/user'>
-                  <p>User</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/home/play">
-                  <p>Play</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/home/about">
-                  <p>About</p>
-                </Link>
-              </li>
-            </ul>
+    <div className="w-full h-20 bg-emerald-800 sticky top-0 shadow-lg">
+      <div className="container mx-auto px-4 h-full">
+        <div className="flex justify-between items-center h-full">
+          <div className="flex items-center gap-x-4">
+            <Image alt="logo" src="/logo.svg" width={50} height={75} className="rounded-full" />
+            <h1 className="text-white text-2xl font-semibold">Heardle</h1>
           </div>
+          <ul className="hidden md:flex gap-x-8 text-white">
+            <li className="hover:text-gray-300 transition duration-300">
+              <Link href="/home">
+                Home
+              </Link>
+            </li>
+            <li className="hover:text-gray-300 transition duration-300">
+              <Link href="/home/user">
+                User
+              </Link>
+            </li>
+            <li className="hover:text-gray-300 transition duration-300">
+              <Link href="/home/play">
+                Play
+              </Link>
+            </li>
+            <li className="hover:text-gray-300 transition duration-300">
+              <Link href="/home/about">
+                About
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
