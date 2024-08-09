1e9604324257707e5156af08c18c4b8dc58c68b6
michael-shu <michaelshu34@gmail.com>
2024-08-08 22:17:20 -0400

    ok that should work

--- app/home/user/User.tsx
+++ app/home/user/User.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Image from 'next/image';
 import path from 'path';
 
-
 interface userProfile {
     display_name: string;
     external_urls: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Link from 'next/link';
+import path from 'path';
 //const spotifyAuthUrl = 'http://localhost:5000/auth/login';
-
-
-
-
 const spotifyAuthURL = process.env.AUTH_URL || '/fallback-url';
+
+
+const spotifyLogin = path.join(spotifyAuthURL, "auth/login");
 
 const page = () => {
 
@@ -14,7 +14,7 @@ const page = () => {
       <div className="bg-white p-10 rounded-lg shadow-lg max-w-md w-full text-center">
         <h1 className="text-4xl font-bold mb-6 text-gray-800">Welcome to Heardle</h1>
         <p className="text-gray-600 mb-6">Sign in with Spotify to start your journey.</p>
-        <Link href={spotifyAuthURL}>
+        <Link href={spotifyLogin}>
           <a className="bg-green-500 text-white py-3 px-6 rounded-full shadow-md hover:bg-green-600 transition duration-300">
             Login with Spotify
           </a>
