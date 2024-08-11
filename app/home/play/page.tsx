ef2c33114ab02d3d42cfb3957340a4d85fb6a836
michael-shu <michaelshu34@gmail.com>
2024-08-11 16:21:55 -0400

    change apostrophes

--- app/home/play/page.tsx
+++ app/home/play/page.tsx
@@ -7,8 +7,8 @@ const page = () => {
   <div className="absolute inset-0 bg-cover bg-center opacity-30" style={{ backgroundImage: "url('/path-to-your-background-image.jpg')" }}></div>
   <div className="relative z-10 text-center px-6">
     <h1 className="text-3xl md:text-5xl font-bold mb-4 animate-pulse">🚧 Work in Progress 🚧</h1>
-    <p className="text-lg md:text-2xl">We&aposre crafting something amazing. Stay tuned!</p>
-    <p className="text-sm md:text-base mt-4">Thank you for your patience. We&aposll be live soon!</p>
+    <p className="text-lg md:text-2xl">We&apos;re crafting something amazing. Stay tuned!</p>
+    <p className="text-sm md:text-base mt-4">Thank you for your patience. We&apos;ll be live soon!</p>
   </div>
 </div>
   )
